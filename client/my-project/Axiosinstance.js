40b69f51cdf2b564b716e56a58701ffc5aa54d22
Akhilesh <akhileshmjamadade@gamil.com>
2026-05-11 11:10:01 +0530

    chore: switch from nodemon to node for production and optimize ML dependencies
    - Use node instead of nodemon for production ser

--- client/my-project/Axiosinstance.js
+++ client/my-project/Axiosinstance.js
@@ -1,7 +1,8 @@
 import axios from "axios"
 
 export const resumeBaseURL = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || (import.meta.env.DEV ? "http://localhost:5000" : "")
+    baseURL: import.meta.env.resume-screening-eosin-sigma.vercel.app
+ |import.meta.env.DE  "http://localhost:5000" : "")
 })
 
 
