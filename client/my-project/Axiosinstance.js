73b94591fb6f097077cfbc3608186c0ac248580a
Akhilesh <akhileshmjamadade@gamil.com>
2026-05-11 10:54:10 +0530

    Fix: Update Axiosinstance to use relative URL for production

--- client/my-project/Axiosinstance.js
+++ client/my-project/Axiosinstance.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const resumeBaseURL = axios.create({
-    baseURL :"http://localhost:5000"
+    baseURL: import.meta.env.VITE_API_URL || (import.meta.env.DEV ? "http://localhost:5000" : "")
 })
 
 
